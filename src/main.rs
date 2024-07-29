38029f15a040e8c3cae3dad203d650c79fce1234
debjit <debjitbhowal.db@gmail.com>
2024-07-29 11:06:35 +0530

    changes for fly.io and perf improvements

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,42 @@
-use anyhow::Result;
-use ic_agent::export::Principal;
-use ic_agent::Agent;
-use ic_agent::identity::Secp256k1Identity;
-// use ic_utils::canister::Canister;
-use tokio;
-use candid::{CandidType, Decode, Deserialize, Encode};
-use rand::Rng;
+mod todo_canister;
+use std::net::SocketAddr;
+use todo_canister::{add_todo, get_todos, initialize};
+// use anyhow::Result;
 
-#[derive(Clone, CandidType, Deserialize, Debug)]
-struct Todo {
-    id: u64,
-    text: String,
-    completed: bool,
-}
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
 
-#[derive(CandidType)]
-struct AddTodoArgs {
-    todos: Vec<String>,
-}
+use serde_json::{json, Value};
+use axum::Json;
 
-#[derive(CandidType, Deserialize)]
-struct  AddTodosResponse {
-    count: u64,
-}
-
-#[derive(CandidType)]
-struct GetTodoArgs {
-    id: u64,
-}
-
-#[derive(CandidType, Deserialize)]
-struct GetTodoResponse {
-    record: Option<Todo>,
-}
-
-pub async fn create_agent(url: &str, is_mainnet: bool) -> Result<Agent> {
-    let agent = Agent::builder()
-        .with_url(url)
-        .with_identity(Secp256k1Identity::from_pem_file("/Users/debjit/.config/dfx/identity/terminal/identity.pem")?)
-        .build()?;
-    if !is_mainnet {
-        agent.fetch_root_key().await?;
-    }
-    Ok(agent)
-}
-
-pub async fn get_todos() -> Vec<u64> {
-    let results = Vec::<u64>::new();
-    results
+async fn hello_world() -> &'static str {
+    "Hello world!"
 }
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let agent = create_agent("https://ic0.app", true).await.unwrap();
-    println!("Agent created");
-    
-    // let canister = Canister::builder()
-    //     .with_agent(&agent)
-    //     .with_canister_id("62nkg-7yaaa-aaaan-qmpla-cai")
-    //     .build()
-    //     .unwrap();
-    
-    let principal = Principal::from_text("62nkg-7yaaa-aaaan-qmpla-cai").unwrap();
-    println!("Principal created\n");
-    // panic!("test");
+    // Initialize the agent and principal
+    initialize().expect("Failed to initialize agent");
 
-    let randomness = rand::thread_rng().gen_range(0..1000);
+    // let todos = get_todos().await;
+    // for todo in &todos {
+    //     println!("{:?}", todo);
+    // }
 
-    let response = agent.update(&principal, "addTodos")
-        .with_arg(Encode!(&vec![format!("Test todo {}", randomness).to_string()]).unwrap())
-        .call_and_wait()
-        .await.unwrap();
-    println!("made addTodo call with randomness: {}", randomness);
-    let count = Decode!(&response, u64).unwrap();
-    println!("count: {:?}\n", count);
+    // let todos = json!(todos.clone());
 
-    let response = agent.query(&principal, "getTodo")
-        .with_arg(Encode!(&((count-1) as u64)).unwrap())
-        .call()
-        .await.unwrap();
-    println!("made getTodo call");
-    let todo = Decode!(&response, Option<Todo>).unwrap().unwrap();
-    println!("todo: {:?}", todo);
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/todos", get(|| async {
+            Json(json!(get_todos().await))
+        }))
+        // .route("/add", get(|| async { Json(add_todo().await) }))
+        ;
+    let addr = SocketAddr::from(([0,0,0,0], 8080));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+    axum::serve(tcp, router).await.unwrap();
+
+    // Ok(())
 }
---
+++ src/todo_canister.rs
@@ -0,0 +1,145 @@
+// pub mod todo_canister {
+    use anyhow::Result;
+    use ic_agent::export::Principal;
+    use ic_agent::Agent;
+    use ic_agent::identity::Secp256k1Identity;
+    use serde::Serialize;
+    use tokio::{self, sync::OnceCell};
+    use candid::{CandidType, Decode, Deserialize, Encode};
+    use rand::Rng;
+
+    #[derive(Clone, CandidType, Deserialize, Debug, Serialize)]
+    pub struct Todo {
+        id: u64,
+        text: String,
+        completed: bool,
+    }
+
+    #[derive(CandidType)]
+    struct AddTodoArgs {
+        todos: Vec<String>,
+    }
+
+    #[derive(CandidType)]
+    struct ToggleTodoArgs {
+        todo_id: u64,
+    }
+
+    #[derive(CandidType, Deserialize)]
+    struct  AddTodosResponse {
+        count: u64,
+    }
+
+    #[derive(CandidType)]
+    struct GetTodoArgs {
+        id: u64,
+    }
+
+    #[derive(CandidType, Deserialize)]
+    struct GetTodoResponse {
+        record: Option<Todo>,
+    }
+
+    #[derive(CandidType)]
+    struct GetPaginatedTodosArgs {
+        offset: u64,
+        limit: u64,
+    }
+
+    // println!("Hello, world!");
+    static AGENT: OnceCell<Agent> = OnceCell::const_new();
+    static PRINCIPAL: OnceCell<Principal> = OnceCell::const_new();
+    // println!("Agent created");
+
+    pub fn initialize() -> Result<()> {
+        println!("Initializing agent");
+        let agent = Agent::builder()
+            .with_url("https://ic0.app")
+            .with_identity(Secp256k1Identity::from_pem_file("./identity.pem")?)
+            .build()?;
+
+        AGENT.set(agent).map_err(|_| anyhow::anyhow!("Agent already initialized"))?;
+        println!("Agent initialized");
+
+        println!("Creating principal");
+        let principal = Principal::from_text("62nkg-7yaaa-aaaan-qmpla-cai").unwrap();
+        PRINCIPAL.set(principal).map_err(|_| anyhow::anyhow!("Principal already initialized"))?;
+        println!("Principal created");
+
+        Ok(())
+    }
+
+    // pub fn create_agent(url: &str) -> Option<Agent> {
+    //     let agent = Agent::builder()
+    //         .with_url(url)
+    //         .with_identity(Secp256k1Identity::from_pem_file("/Users/debjit/.config/dfx/identity/terminal/identity.pem").expect("Unable to read pem file"))
+    //         .build().expect("Unable to create agent");
+    //     Some(agent)
+    // }
+
+    pub async fn get_todos() -> Vec<Todo> {
+        let response = AGENT.get().expect("AGENT not initialized").query(&PRINCIPAL.get().expect("PRINCIPAL not set"), "getPaginatedTodos")
+            .with_arg(Encode!(&(0 as u64), &(100 as u64)).unwrap())
+            .call()
+            .await.unwrap();
+        Decode!(&response, Vec<Todo>).unwrap()
+    }
+
+    pub async fn add_todo() -> u64 {
+        let randomness = rand::thread_rng().gen_range(0..1000);
+        let response = AGENT.get().expect("AGENT not initialized").update(&PRINCIPAL.get().expect("PRINCIPAL not set"), "addTodos")
+        .with_arg(Encode!(&vec![format!("Test todo {}", randomness).to_string()]).unwrap())
+            .call_and_wait()
+            .await.unwrap();
+        Decode!(&response, u64).unwrap()
+    }
+
+    pub async fn get_todo_by_id(id: u64) -> Option<Todo> {
+        let response = AGENT.get().expect("AGENT not initialized").query(&PRINCIPAL.get().expect("PRINCIPAL not set"), "getTodo")
+            .with_arg(Encode!(&GetTodoArgs { id }).unwrap())
+            .call()
+            .await.unwrap();
+        Decode!(&response, GetTodoResponse).unwrap().record
+    }
+
+    pub async fn toggle_todo_by_id(todo_id: u64) -> Option<Todo> {
+        let response = AGENT.get().expect("AGENT not initialized").update(&PRINCIPAL.get().expect("PRINCIPAL not set"), "toggleTodo")
+            .with_arg(Encode!(&(todo_id as u64)).unwrap())
+            .call_and_wait()
+            .await.unwrap();
+        Decode!(&response, GetTodoResponse).unwrap().record
+    }
+
+    // #[tokio::main]
+    // async fn main() {
+        
+        // let canister = Canister::builder()
+        //     .with_agent(&agent)
+        //     .with_canister_id("62nkg-7yaaa-aaaan-qmpla-cai")
+        //     .build()
+        //     .unwrap();
+        // let agent = create_agent("https://ic0.app").unwrap();
+        
+        // let principal = Principal::from_text("62nkg-7yaaa-aaaan-qmpla-cai").unwrap();
+        // println!("Principal created\n");
+        // panic!("test");
+
+        // let randomness = rand::thread_rng().gen_range(0..1000);
+
+        // let response = agent.update(&principal, "addTodos")
+        //     .with_arg(Encode!(&vec![format!("Test todo {}", randomness).to_string()]).unwrap())
+        //     .call_and_wait()
+        //     .await.unwrap();
+        // println!("made addTodo call with randomness: {}", randomness);
+        // let count = Decode!(&response, u64).unwrap();
+        // println!("count: {:?}\n", count);
+
+        // let response = agent.query(&principal, "getTodo")
+        //     .with_arg(Encode!(&((count-1) as u64)).unwrap())
+        //     .call()
+        //     .await.unwrap();
+        // println!("made getTodo call");
+        // let todo = Decode!(&response, Option<Todo>).unwrap().unwrap();
+        // println!("todo: {:?}", todo);
+    // }
+// }
